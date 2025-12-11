c609bfb107f471f4dd00a5930be48b9c82ee748d
traliach <ali.achille.traore@gmail.com>
2025-12-11 14:48:40 -0500

    add longtestring function

--- main.js
+++ main.js
@@ -21,4 +21,32 @@ const sampleNumbers2 = [10, -5, 7, 3];
 console.log("Sum of", sampleNumbers1, "is", sumNumbers(sampleNumbers1));
 console.log("Sum of", sampleNumbers2, "is", sumNumbers(sampleNumbers2));
 
+// output:
+// Sum of [ 1, 2, 3 ] is 6
+// Sum of [ 10, -5, 7, 3 ] is 15
+
+// Find the longest string in an array.
+// If there is a tie, this returns the first longest string.
+function longestString(strings) {
+  if (strings.length === 0) {
+    return ""; // simple choice for empty array
+  }
+
+  let longest = strings[0];
+
+  for (const str of strings) {
+    if (str.length > longest.length) {
+      longest = str;
+    }
+  }
+
+  return longest;
+}
+
+// Quick tests for longestString
+const words1 = ["say", "hello", "in", "the", "morning"];
+const words2 = ["cat", "hippopotamus", "dog", "giraffe"];
+
+console.log("Longest in", words1, "is", longestString(words1));
+console.log("Longest in", words2, "is", longestString(words2));
 
